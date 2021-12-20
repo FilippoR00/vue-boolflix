35907eff028f22699b7730a82f875e99411e618e
Filippo Rossi <rossifilippo2000@gmail.com>
2021-12-20 16:53:59 +0100

    MILESTONE 1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,41 @@
 <template>
 	<div>
-		<img src="../assets/logo.png" alt="Logo">
+		<img src="..\assets\logo.png" alt="Logo">
+		<input type="text" v-model="search" v-on:keyup.enter="getMovies">
+		<button @click='getMovies'>Cerca</button>
 	</div>
 </template>
 
 <script>
+import axios from 'axios';
+import dataShared from "../share/dataShared";
+
 export default {
-	name: 'Header'
+	name: 'Header',
+	data() {
+		return {
+			search : '',
+			dataShared,
+
+		}
+	},
+	methods: {
+		getMovies() {
+			axios.get('https://api.themoviedb.org/3/search/movie', {
+				params: {
+					api_key : '0b9ac5d0a41cb0ca49e6d38df58bc461',
+					language : 'it-IT',
+					query : this.search
+				}
+			})
+			.then(function (response) {
+				dataShared.myData = response.data.results;
+			})
+			.catch(function (error) {
+				console.log(error);
+			});  
+		}
+	},
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        
+        <div v-for="(datas, index) in dataShared.myData" :key="index">
+            <div class="title">TITOLO: {{datas.title}}</div>
+            <div class="original-title">TITOLO ORIGINALE: {{datas.original_title}}</div>
+            <div class="language">LINGUA: {{datas.original_language}}</div>
+            <div class="rating">VOTO: {{datas.vote_average}}</div>
+        </div>
     </div>
 </template>
 
 <script>
+import dataShared from "../share/dataShared";
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    data() {
+        return {
+            dataShared,
+
+        }
+    },
 }
 </script>
 
